75a71a462a63e32f554a2af18708a47ae46badf7
Daniyal Azram <daniazram@gmail.com>
2021-09-10 12:38:02 +0500

    Fixed the popup selection reset on reload issue

--- Assets/BaseProject/Scripts/MVVM/Editor/DataBinderEditor.cs
+++ Assets/BaseProject/Scripts/MVVM/Editor/DataBinderEditor.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEditor;
+
+using System;
 using System.Linq;
+using System.Collections.Generic;
 
 [CustomEditor(typeof(DataBinder))]
 public class DataBinderEditor : Editor
@@ -18,15 +21,20 @@ public class DataBinderEditor : Editor
         var sourcePropsData = PropertyFinderHelper.GetSourceProperties(instance.gameObject);
         var sourceOptions = sourcePropsData.Select(data => new GUIContent(string.Concat(data.type.Name, "/", data.property.PropertyType.Name, " : ", data.property.Name))).ToArray();
 
-        DrawPopup(ref targetPropIndex, new GUIContent("Target Property"), targetOptions);
-        DrawPopup(ref sourceValueIndex, new GUIContent("Source Value"), sourceOptions);
+        DrawPopup(ref targetPropIndex, new GUIContent("Target Property"), targetOptions, GetCurrentIndexFrom(instance.TargetProperty, targetPropsData));
+        DrawPopup(ref sourceValueIndex, new GUIContent("Source Value"), sourceOptions, GetCurrentIndexFrom(instance.SourceValue, sourcePropsData));
 
         instance.TargetProperty = targetPropsData.ElementAt(targetPropIndex).FullReference;
         instance.SourceValue = sourcePropsData.ElementAt(sourceValueIndex).FullReference;
     }
 
-    private void DrawPopup(ref int index, GUIContent label, GUIContent[] options)
+    private void DrawPopup(ref int index, GUIContent label, GUIContent[] options, int currentIndex)
     {
-        index = EditorGUILayout.Popup(label, index, options);
+        index = EditorGUILayout.Popup(label, currentIndex, options);
+    }
+
+    private int GetCurrentIndexFrom(string currentValue, IEnumerable<BindableMemberData> propsData)
+    {
+        return propsData.Select(data => data.FullReference).ToList().IndexOf(currentValue);
     }
 }
